a369dd7c1e19d1189b6084551d5c491f3246cdac
ksorel <sorel.kone@gmail.com>
2026-05-26 10:59:44 +0000

    Config Firebase correcte

--- public/js/config.js
+++ public/js/config.js
@@ -1,23 +1,24 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp }
+  from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
+import { getFirestore }
+  from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
+import { getAuth }
+  from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js';
+import { getStorage }
+  from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js';
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCU4Mqn6Wfyy5irKh9DpVoXFasx2N0-gGE",
-    authDomain: "delices-etoiles.firebaseapp.com",
-    projectId: "delices-etoiles",
-    storageBucket: "delices-etoiles.firebasestorage.app",
-    messagingSenderId: "795728972354",
-    appId: "1:795728972354:web:50403340daee555d66fbdb",
-    measurementId: "G-NGFS8MC8CW"
-  };
+const firebaseConfig = {
+  apiKey:            "AIzaSyCU4Mqn6Wfyy5irKh9DpVoXFasx2N0-gGE",
+  authDomain:        "delices-etoiles.firebaseapp.com",
+  projectId:         "delices-etoiles",
+  storageBucket:     "delices-etoiles.firebasestorage.app",
+  messagingSenderId: "795728972354",
+  appId:             "1:795728972354:web:50403340daee555d66fbdb"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const app = initializeApp(firebaseConfig);
+
+export const db      = getFirestore(app);
+export const auth    = getAuth(app);
+export const storage = getStorage(app);
+export default app;
