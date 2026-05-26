916fc646ee09ebb568891ef0697200018be9e3f5
ksorel <sorel.kone@gmail.com>
2026-05-26 10:53:27 +0000

    Ajout config.js Firebase CDNbon

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,23 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCU4Mqn6Wfyy5irKh9DpVoXFasx2N0-gGE",
-  authDomain: "delices-etoiles.firebaseapp.com",
-  projectId: "delices-etoiles",
-  storageBucket: "delices-etoiles.firebasestorage.app",
-  messagingSenderId: "795728972354",
-  appId: "1:795728972354:web:50403340daee555d66fbdb",
-  measurementId: "G-NGFS8MC8CW"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCU4Mqn6Wfyy5irKh9DpVoXFasx2N0-gGE",
+    authDomain: "delices-etoiles.firebaseapp.com",
+    projectId: "delices-etoiles",
+    storageBucket: "delices-etoiles.firebasestorage.app",
+    messagingSenderId: "795728972354",
+    appId: "1:795728972354:web:50403340daee555d66fbdb",
+    measurementId: "G-NGFS8MC8CW"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
